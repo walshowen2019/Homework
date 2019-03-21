02865e7c60ba9f1375bbed27f7674bc075e41dce
walshowen2019 <42677886+walshowen2019@users.noreply.github.com>
2019-03-21 00:10:03 -0700

    Create Main.java

---
+++ Cash Register/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Product fujiApples = new Product("Fuji Apples",4131,false,.49,75,"Produce");
+        Product apricots = new Product("Apricots",4218,false,.39,75,"Produce");
+    }
+}
